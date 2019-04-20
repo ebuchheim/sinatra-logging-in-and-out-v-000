58785dc345180306f0cd0e6f7142df2d8b221ed2
ebuchheim <emma.buchheim@gmail.com>
2019-04-19 20:41:31 -0700

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,19 +7,32 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    session.clear
     erb :index
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params["username"])
+    if @user.nil?
+      erb :error
+    else
+      session[:user_id] = @user.id
+      redirect '/account'
+    end
   end
 
   get '/account' do
-
+    @current_user = Helpers.current_user(session)
+    if !!Helpers.is_logged_in?(session)
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
